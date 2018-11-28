b3e87db162ccb7a18caae46964e5c59e54afb6fd
ldm1294apple <ldm1294apple@gmail.com>
2018-11-28 16:56:25 +0100

    fdsfws

--- Statistics/Controller/BarChartsViewController.swift
+++ Statistics/Controller/BarChartsViewController.swift
@@ -55,6 +55,8 @@ class BarChartsViewController: UIViewController , UIPickerViewDelegate, UIPicker
         }
         pickerView.dataSource = self
         pickerView.delegate = self
+        let distribution =  TriangolarDistribution.init(a: 2, b: 100, c: 40)
+        printDistribution(distribution: distribution!)
         // Do any additional setup after loading the view.
     }
     
@@ -64,36 +66,62 @@ class BarChartsViewController: UIViewController , UIPickerViewDelegate, UIPicker
         }
     }
     
+    func printDistribution(distribution : ProbabilityDistribution){
+        barChartsView.clear()
+        barChartsView.clearValues()
+        var time : Double = 0
+        var dataEntries : [BarChartDataEntry] = []
+        while time < 100 {
+            let entry = BarChartDataEntry(x : time, y: distribution.probabiltyAtTime(time: time))
+            dataEntries.append(entry)
+            time += 0.01
+        }
+        let dataSet = BarChartDataSet.init(values: dataEntries, label: distribution.name)
+        let data = BarChartData.init(dataSets: [dataSet])
+        barChartsView.data = data
+    }
+    
     
 
     @IBAction func startButtonPressed(_ sender: UIButton) {
         numberTextField.resignFirstResponder()
         if let numberTextField = numberTextField, let text = numberTextField.text, let value = Int(text) {
             sender.isEnabled = false
+            //graphic code
             if let activity = activityIndicator {
                 activity.isHidden = false
                 activity.startAnimating()
             }
             let row = pickerView.selectedRow(inComponent: 0)
+//            I select the generator of RV according to the user choice
             let randomVariables : RandomVariableGenerator = dictionary[keysArray[row]]!
+//            I recall the parallel generator, class used to generate RV in parallel
+//            so i will not stop the main thread to perform this task
             parallelGenerator = ParallelDistributionGenerator(randomGenerator: randomVariables)
+//            If parallel generator object was correctly generated i use it.
             if let parallel = parallelGenerator {
                 parallel.generate(number: value) { (distributions) in
+                    //after the completion of the task i manage the graphics
+                    //distributions contains the array of distributions generated
                     if let activity = self.activityIndicator {
                         activity.stopAnimating()
                         activity.isHidden = true
                     }
                     sender.isEnabled = true
+                    //i create an array to be displayed on the graphic
+                    //function is a reference containing the function i am displaying,
+                    // in this case average.
                     var array : [(lambda : Int,function :  () -> Double)] = []
                     for i in 0..<distributions.count {
                         array.append((i, distributions[i].average))
                     }
                     
+                    //showing on the graph
                     self.barChartsView.clearValues()
                     self.barChartsView.clear()
                     var dataEntry : [BarChartDataEntry] = []
                     for i in array {
-                        let entry = BarChartDataEntry.init(x: Double(i.lambda), y: i.function())
+                        let entry = BarChartDataEntry(x: Double(i.lambda), y: i.function())
                         dataEntry.append(entry)
                     }
                     let chartDataSet = BarChartDataSet(values: dataEntry, label: distributions[0].name)
